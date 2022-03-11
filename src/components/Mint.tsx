ace577097795bbd40e7870d18b3c1c5d98d2b43f
Greg Tordjman <gtordjma@student.42.fr>
2022-03-11 15:50:09 +0100

    v2

--- src/components/Mint.tsx
+++ src/components/Mint.tsx
@@ -1,5 +1,9 @@
+import { utils } from 'ethers'
+import { Contract } from '@ethersproject/contracts'
+import abi from '../contract/abi.json';
 import { ContractCall, useContractFunction, useEthers, useContractCall } from '@usedapp/core';
-
+import { Button, Typography } from '@material-ui/core';
+import { useState } from 'react';
 
 export const Mint = () => {
 
@@ -7,25 +11,76 @@ export const Mint = () => {
     const { chainId, account } = useEthers()
     const isConnected = account !== undefined
 
+    const wethInterface = new utils.Interface(abi.abi)
+    const wethContractAddress = '0x1d2175026745d2bB9b4A270312feC02Ff016b2D3'
+    const contract = new Contract(wethContractAddress, wethInterface)
+
+    const totalMinted = useContractCall({
+        abi: wethInterface,
+        address: wethContractAddress,
+        method: "totalSupply",
+        args: []
+    })
+
+    const { send: mintSend, state: mintState } = useContractFunction(contract, 'mint', {
+        transactionName: "Mint",
+    })
+
+    const mint = () => {
+        const check = Number(totalMinted)
+        const amount = (check <= 255 ? 0 : numberOfMint * 15000000000000000)
+        mintSend(1, { value: amount.toString() })
+    }
+
+    const [numberOfMint, setNumberOfMint] = useState(5);
+
+    const lowerNumberMint = () => {
+        if (numberOfMint > 0)
+            setNumberOfMint(numberOfMint - 1);
+    }
+    const upperNumberMint = () => {
+        if (numberOfMint < 5)
+            setNumberOfMint(numberOfMint + 1);
+    }
+
+
 
     return (
 
         <>
-            {
-                isConnected ? (
-                    chainId != 1 ? (<p>Please Connect to the Mainnet Network</p>) : (
-                        <div className="mint-box">
-                            <div className=''>
-                                <div className='upbutton'>
-                                    <button className='button'>FREE MINT SOON</button>
-                                </div>
-                            </div>
-                        </div >
-                    )) : (
-                    <></>
-                )
+            <div className="mint-box">
+                <div className=''>
+                    {
+                        isConnected ? (
+                            chainId != 1 ? (<p>Please Connect to the Mainnet Network</p>) : (
 
-            }
+                                <div className='upbutton'>
+                                    <p>Total Azukevin: 555</p>
+                                    <p>First 255 Free, 0.015ETH then</p><br /><br />
+                                    <p>{mintState.status === 'None' ? "" : "Mint status: " + mintState.status}</p>
+                                    <p>{mintState.errorMessage}</p>
+                                    <div className='nbmint'>
+                                        <Button onClick={lowerNumberMint}><span >-</span></Button>
+                                        <Typography variant="body1" >{numberOfMint}</Typography>
+                                        <Button onClick={upperNumberMint}><span >+</span></Button>
+                                        <Button className='button' onClick={mint}>Mint</Button>
+                                    </div>
+
+                                    <p>Total minted: {totalMinted ? totalMinted.toString() : 0} / 555</p>
+                                </div>
+                            )) : (
+                            <div className='upbutton'>
+                                <p>Please connect your wallet</p><br /><br />
+                                <p>Total Azukevin: 555</p>
+                                <p>First 255 Free, 0.015ETH then</p>
+                            </div>
+                        )
+
+
+                    }
+
+                </div>
+            </div>
         </>
 
 
